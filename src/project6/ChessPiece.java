4e357864b098d0dd824c805190e14ea835b86eec
Anurag Andoji <Andoji@wireless-10-145-208-72.public.utexas.edu>
2015-11-23 15:44:57 -0600

    ChessPiece abstract

---
+++ src/project6/ChessPiece.java
@@ -0,0 +1,17 @@
+package project6;
+
+public abstract class ChessPiece {
+	
+	private int value;
+	
+	/*True = white, False = black*/
+	private boolean color;
+	
+	private int x;
+	private int y;
+	
+	public abstract void move(int row, int col);
+	
+	public abstract void showMoves();
+	
+}
