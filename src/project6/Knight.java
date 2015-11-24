ddbd8bfe0f622eeb7ff1888a484ebc3fdf56bcec
kingjkang <justin.d.kang@gmail.com>
2015-11-24 13:30:49 -0600

    knignt

--- src/project6/Knight.java
+++ src/project6/Knight.java
@@ -10,7 +10,70 @@ public class Knight extends ChessPiece{
 		ChessBoard[x][y] = this;
 	}
 	
+	public String toString(){
+		return "K";
+	}
+	
 	public boolean isValidMove(int nx, int ny) {
+		//this checks to see if it is wininn the range/scope of the chessboard
+		if(nx > 7 || nx < 0 || ny > 7 || ny < 0){
+			return false;
+		}
+		
+		//checking the 8 valid moves 
+		if (nx == (x+1) && ny == (y-2)){
+			//top right move
+			//and check if the move will put the king in check
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		else if (nx == (x+2) && ny == (y-1)){
+			//right right move
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		else if (nx == (x+2) && ny == (y+1)){
+			//right right move
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		else if (nx == (x+1) && ny == (y+2)){
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		else if (nx == (x-1) && ny == (y+2)){
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		else if (nx == (x-2) && ny == (y+1)){
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		else if (nx == (x-2) && ny == (y-1)){
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		else if (nx == (x-1) && ny == (y-2)){
+			if (inCheck(nx, ny)){
+				return false;
+			}
+			return true;
+		}
+		
 		return false;
 	}
 
