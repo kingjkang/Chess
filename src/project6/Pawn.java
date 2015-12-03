6a0236a13db0b1a8a0578b357d71031f6e2fa440
Anurag Andoji <Andoji@wireless-10-145-210-6.public.utexas.edu>
2015-12-03 12:05:39 -0600

    pawn

--- src/project6/Pawn.java
+++ src/project6/Pawn.java
@@ -141,11 +141,6 @@ public class Pawn extends ChessPiece{
 		}
 
 		
-		
-//		if(inCheck(this.row, this.col, r, c)){
-//			return false;
-//		}
-		
 		if(inCheck(r, c)){
 			return false;
 		}
@@ -199,6 +194,9 @@ public class Pawn extends ChessPiece{
 				else{
 					return false;
 				}
+			}
+			else{
+				return false;
 			}
 		}
 		else{
