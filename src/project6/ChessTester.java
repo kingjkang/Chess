3191fa19d02ff2943ef314ddfcfc95e0741c31da
Anurag Andoji <Andoji@wireless-10-147-33-173.public.utexas.edu>
2015-12-04 11:05:07 -0600

    JUnit

---
+++ src/project6/ChessTester.java
@@ -0,0 +1,14 @@
+package project6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChessTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
